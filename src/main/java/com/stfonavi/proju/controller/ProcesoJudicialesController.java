a906dcabc282dc52f961f6a293110b80b8c5d861
Edwin HC <edwinrhc@gmail.com>
2024-10-24 17:06:20 -0500

    ModuloProcesos Judiciales

--- src/main/java/com/stfonavi/proju/controller/ProcesoJudicialesController.java
+++ src/main/java/com/stfonavi/proju/controller/ProcesoJudicialesController.java
@@ -1,11 +1,21 @@
 package com.stfonavi.proju.controller;
 
+import com.stfonavi.proju.model.entity.ProcesoJudiciales;
 import com.stfonavi.proju.model.service.interfaces.IProcesoJudicialesService;
+import com.stfonavi.proju.util.helper.Constantes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/procesoJudiciales")
@@ -20,6 +30,36 @@ public class ProcesoJudicialesController {
         return "uap/procesoJudiciales/procesoJudicialesView";
     }
 
+
+    @GetMapping("/get")
+    public String mostrarForm(Map<String,Object> model){
+
+        ProcesoJudiciales procesoJudiciales = new ProcesoJudiciales();
+        model.put("procesoJudiciales",procesoJudiciales);
+        model.put("boton", Constantes.botonNuevo);
+        model.put("titulo",Constantes.registroForm);
+
+        return "uap/procesoJudiciales/formProcesoJudiciales";
+    }
+
+
+    @PostMapping("/crear")
+    public String guardar(@Valid @ModelAttribute("procesoJudiciales") ProcesoJudiciales procesoJudiciales, BindingResult result,
+                          Model model, RedirectAttributes flash, SessionStatus status){
+
+        if(result.hasErrors()){
+            model.addAttribute("procesoJudiciales",procesoJudiciales);
+            model.addAttribute("titulo",Constantes.registroForm);
+            model.addAttribute("boton",Constantes.botonNuevo);
+            return "uap/procesoJudiciales/formProcesoJudiciales";
+        }
+        procesoJudicialesService.save(procesoJudiciales);
+        String mensajeFlash = (procesoJudiciales.getIdProcesoJudicial() != 0) ? "Editar Correctamente": "Creado Correctamente";
+        status.setComplete();
+        flash.addFlashAttribute("success",mensajeFlash);
+
+        return "redirect:/procesoJudiciales/view";
+    }
 
 
 }
